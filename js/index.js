dbd2fa65abb0a072120f19400cf7b3d3ed903de8
KlartNET <36400787+JadeMin@users.noreply.github.com>
2022-12-09 22:38:17 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ Array.prototype.getRandom = function(){
 	return this[Math.floor(Math.random() * this.length)];
 };
 const randlist = ["https://www.youtube.com/watch?v=JCaP11BfiGw", "https://www.youtube.com/watch?v=JCaP11BfiGw"];
-const $img = $("#cataas-img");
+const $img = $("#dogaas-img");
 
 jQuery(document).ready(async ()=> {
 	const response = await fetch("https://dog.ceo/api/breeds/image/random");
