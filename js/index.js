b76d924eb96464e8cea4a028a9f579ea2701d803
KlartNET <36400787+JadeMin@users.noreply.github.com>
2022-12-09 22:39:25 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 Array.prototype.getRandom = function(){
 	return this[Math.floor(Math.random() * this.length)];
 };
-const randlist = ["https://www.youtube.com/watch?v=JCaP11BfiGw", "https://www.youtube.com/watch?v=JCaP11BfiGw"];
+const rickrolls = ["https://www.youtube.com/watch?v=JCaP11BfiGw", "https://www.youtube.com/watch?v=JCaP11BfiGw"];
 const $img = $("#dogaas-img");
 
 jQuery(document).ready(async ()=> {
 	const response = await fetch("https://dog.ceo/api/breeds/image/random");
 	$img.attr('src', (await response.json()).message);
 	
-	$img.click(() => location.href = arr.getRandom());
+	$img.click(() => location.href = rickrolls.getRandom());
 });
