2fb109ac7674c97ba2ef02da51f12181b697fde6
choroogyee <47732670+choroogyee@users.noreply.github.com>
2023-01-20 06:22:51 +0900

    Update keyevent.js

--- js/keyevent.js
+++ js/keyevent.js
@@ -13,7 +13,8 @@ document.onmousedown=disableclick;
 function disableclick(e){
     if(e.button==2) {
         alert('저작권 보호를 위해 우클릭은 사용하실수 없습니다.');
-        return false;
+        e.preventDefault();
+        e.returnValue = false;
     }
 }
 
