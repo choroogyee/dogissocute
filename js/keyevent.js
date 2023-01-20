41e0df250758a1342b0d4a63475608617c19da86
choroogyee <47732670+choroogyee@users.noreply.github.com>
2023-01-20 13:14:44 +0900

    Update keyevent.js

--- js/keyevent.js
+++ js/keyevent.js
@@ -1,22 +1,61 @@
 $(document).ready(function(){
-    $(document).bind('keydown', function(e){
-        if(e.keyCode == 123) {
-            alert('사이트 보호를위해 개발자도구 사용을 금지합니다.');
-            e.preventDefault();
-            e.returnValue = false;
-        }
-    });
+	$(document).on('keydown', function(e){
+		if(e.keyCode == 123) {
+			swal({
+				title: '경고!',
+				text: '사이트 보호를 위해 개발자도구를 사용하실수 없습니다.',
+				icon: 'warning'
+			});
+			e.preventDefault();
+			return false;
+		}
+
+		if(e.keyCode == 17) {
+			swal({
+				title: '경고!',
+				text: '사이트 보호를 위해 ctrl키를 사용하실수 없습니다.',
+				icon: 'warning'
+			});
+			e.preventDefault();
+			return false;
+		}
+
+		if(e.keyCode == 16) {
+			swal({
+				title: '경고!',
+				text: '사이트 보호를 위해 shift키를 사용하실수 없습니다.',
+				icon: 'warning'
+			});
+			e.preventDefault();
+			return false;
+		}
+
+		if(e.keyCode == 73) {
+			swal({
+				title: '경고!',
+				text: '사이트 보호를 위해 i키를 사용하실수 없습니다.',
+				icon: 'warning'
+			});
+			e.preventDefault();
+			return false;
+		}
+	});
+   	$("#cataas-img").on('contextmenu', event => {
+		swal({
+			title: '경고!',
+			text: '이미지 저작권 보호를위해 우클릭을 사용할수 없습니다.',
+			icon: 'warning'
+		});
+		event.preventDefault();
+		return false;
+	});
+	$("#cataas-img").on('dragstart', event => {
+		swal({
+			title: '경고!',
+			text: '이미지 저작권 를 위해 드래그를 사용할수 없습니다.',
+			icon: 'warning'
+		});
+		event.preventDefault();
+		return false;
+	});
 });
-
-document.onmousedown=disableclick;
-
-function disableclick(e){
-    if(e.button==2) {
-        alert('저작권 보호를 위해 우클릭은 사용하실수 없습니다.');
-        e.preventDefault();
-        e.returnValue = false;
-    }
-}
-
-document.ondragstart = 
-    function(){alert('저작권 방지를위해 드래그는 사용하실수 없습니다.'); return false;}
